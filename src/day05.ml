204e162fc2a4525304db22186123b71b2053c7e7
Levi Roth <levimroth@gmail.com>
2020-12-05 08:33:31 -0500

    Simplify day 5

--- src/day05.ml
+++ src/day05.ml
@@ -2,52 +2,48 @@ open! Core
 open! Import
 
 module Seat = struct
-  type t = Int_pair.t [@@deriving sexp]
+  type t = int [@@deriving sexp]
 
   let parser =
-    let open Angstrom in
-    let as_bit ~true_ ~false_ = char true_ *> return 1 <|> char false_ *> return 0 in
-    let to_int bits = List.fold bits ~init:0 ~f:(fun n bit -> (n lsl 1) + bit) in
-    map2
-      (count 7 (as_bit ~true_:'B' ~false_:'F'))
-      (count 3 (as_bit ~true_:'R' ~false_:'L'))
-      ~f:(fun row col -> to_int row, to_int col)
+    Angstrom.scan_state 0 (fun n c ->
+        Option.map
+          ~f:(( + ) (n lsl 1))
+          (match c with
+          | 'B' | 'R' -> Some 1
+          | 'F' | 'L' -> Some 0
+          | _ -> None))
   ;;
 end
 
 module Common = struct
   module Input = Input.Make_parseable_many (Seat)
   module Output = Int
-
-  let to_id (row, col) = (8 * row) + col
 end
 
 let%expect_test _ =
   let test_cases = [ "FBFBBFFRLR"; "BFFFBBFRRR"; "FFFBBBFRRR"; "BBFFBBFRLL" ] in
   let open Common in
   List.iter test_cases ~f:(fun input ->
-      let seat = Input.Single.of_string input in
-      let id = to_id seat in
-      print_s [%sexp { input : string; seat : Seat.t; id : int }]);
+      let id = Input.Single.of_string input in
+      print_s [%sexp { input : string; id : int }]);
   [%expect
     {|
-    ((input FBFBBFFRLR) (seat (44 5)) (id 357))
-    ((input BFFFBBFRRR) (seat (70 7)) (id 567))
-    ((input FFFBBBFRRR) (seat (14 7)) (id 119))
-    ((input BBFFBBFRLL) (seat (102 4)) (id 820)) |}]
+    ((input FBFBBFFRLR) (id 357))
+    ((input BFFFBBFRRR) (id 567))
+    ((input FFFBBBFRRR) (id 119))
+    ((input BBFFBBFRLL) (id 820)) |}]
 ;;
 
 module Part_01 = struct
   include Common
 
-  let solve i = List.map i ~f:to_id |> List.max_elt ~compare |> Option.value_exn
+  let solve i = List.max_elt i ~compare |> Option.value_exn
 end
 
 module Part_02 = struct
   include Common
 
-  let solve i =
-    let ids = List.map i ~f:to_id in
+  let solve ids =
     let min = List.min_elt ids ~compare |> Option.value_exn in
     let max = List.max_elt ids ~compare |> Option.value_exn in
     let set = Int.Hash_set.of_list ids in
