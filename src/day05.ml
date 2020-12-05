94de3e6c980c4990c62549c17e739a91af6be1db
Levi Roth <levimroth@gmail.com>
2020-12-05 07:50:42 -0500

    Solution for Day 5, part 1

---
+++ src/day05.ml
@@ -0,0 +1,36 @@
+open! Core
+open! Import
+
+module Common = struct
+  module Input = Input.Make_parseable_many (struct
+    type t = int
+
+    let parser =
+      let scan_and_shift string ~true_ ~false_ =
+        String.fold string ~init:0 ~f:(fun n c ->
+            (n lsl 1)
+            +
+            match Char.equal c true_ with
+            | true -> 1
+            | false ->
+              (match Char.equal c false_ with
+              | true -> 0
+              | false -> raise_s [%message "Unexpected char" (c : char) (string : string)]))
+      in
+      let open Angstrom in
+      map2 (take 7) (take 3) ~f:(fun row col ->
+          (8 * scan_and_shift row ~true_:'B' ~false_:'F')
+          + scan_and_shift col ~true_:'R' ~false_:'L')
+    ;;
+  end)
+
+  module Output = Int
+end
+
+module Part_01 = struct
+  include Common
+
+  let solve i = List.max_elt i ~compare |> Option.value_exn
+end
+
+let parts : (module Solution.Part) list = [ (module Part_01) ]
