d5e7b9378fad4b568d12a8957a0ea2331f2de94f
Levi Roth <levimroth@gmail.com>
2020-12-08 08:18:46 -0500

    Day 8, part 2

--- src/day08.ml
+++ src/day08.ml
@@ -51,24 +51,32 @@ module Common = struct
   module Output = Int
 end
 
+let run program =
+  let visited = Int.Hash_set.create () in
+  let rec loop ~index ~accumulator =
+    match Hash_set.mem visited index with
+    | true -> `Looped accumulator
+    | false ->
+      Hash_set.add visited index;
+      (match program.(index) with
+      | exception Invalid_argument _ -> `Terminated accumulator
+      | ({ operation; argument } : Instruction.t) ->
+        (match operation with
+        | Accumulate -> loop ~index:(index + 1) ~accumulator:(accumulator + argument)
+        | Jump -> loop ~index:(index + argument) ~accumulator
+        | No_op -> loop ~index:(index + 1) ~accumulator))
+  in
+  loop ~index:0 ~accumulator:0
+;;
+
 module Part_01 = struct
   include Common
 
   let solve input =
     let program = List.to_array input in
-    let visited = Int.Hash_set.create () in
-    let rec loop ~index ~accumulator =
-      match Hash_set.mem visited index with
-      | true -> accumulator
-      | false ->
-        Hash_set.add visited index;
-        let ({ operation; argument } : Instruction.t) = program.(index) in
-        (match operation with
-        | Accumulate -> loop ~index:(index + 1) ~accumulator:(accumulator + argument)
-        | Jump -> loop ~index:(index + argument) ~accumulator
-        | No_op -> loop ~index:(index + 1) ~accumulator)
-    in
-    loop ~index:0 ~accumulator:0
+    match run program with
+    | `Looped accumulator -> accumulator
+    | `Terminated _ -> failwith "Unexpected termination"
   ;;
 end
 
@@ -86,4 +94,37 @@ acc +6|} in
   [%expect {| 5 |}]
 ;;
 
-let parts : (module Solution.Part) list = [ (module Part_01) ]
+module Part_02 = struct
+  include Common
+
+  let solve (input : Input.t) =
+    let run_with_modified_operation index operation =
+      let program = List.to_array input in
+      program.(index) <- { (program.(index)) with operation };
+      match run program with
+      | `Looped _ -> None
+      | `Terminated accumulator -> Some accumulator
+    in
+    List.find_mapi_exn input ~f:(fun index instruction ->
+        match instruction.operation with
+        | Accumulate -> None
+        | Jump -> run_with_modified_operation index No_op
+        | No_op -> run_with_modified_operation index Jump)
+  ;;
+end
+
+let%expect_test _ =
+  let test_case = {|nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6|} in
+  Part_02.Input.of_string test_case |> Part_02.solve |> printf "%d\n";
+  [%expect {| 8 |}]
+;;
+
+let parts : (module Solution.Part) list = [ (module Part_01); (module Part_02) ]
