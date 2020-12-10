f8901f2bbce6f5a64fe7135dbbf47ce6f597dee0
Levi Roth <levimroth@gmail.com>
2020-12-10 18:15:26 -0500

    Day 10, part 2

--- src/day10.ml
+++ src/day10.ml
@@ -24,8 +24,7 @@ module Part_01 = struct
   ;;
 end
 
-let%expect_test _ =
-  let test_case = {|16
+let test_case = {|16
 10
 15
 5
@@ -35,10 +34,40 @@ let%expect_test _ =
 19
 6
 12
-4|} in
+4|}
+
+let%expect_test _ =
   let input = Part_01.Input.of_string test_case in
   print_s [%sexp (Part_01.solve input : int)];
   [%expect {| 35 |}]
 ;;
 
-let parts : (module Solution.Part) list = [ (module Part_01) ]
+module Part_02 = struct
+  include Common
+
+  let solve input =
+    let final = Option.value_exn (List.max_elt input ~compare) + 3 in
+    let in_order = List.concat [ List.sort input ~compare; [ final ] ] in
+    let paths_to_numbers =
+      List.fold in_order ~init:(Int.Map.singleton 0 1) ~f:(fun paths_to_numbers n ->
+          let previous = List.range (n - 3) n in
+          let paths_to_n =
+            List.sum
+              (module Int)
+              previous
+              ~f:(fun previous ->
+                Map.find paths_to_numbers previous |> Option.value ~default:0)
+          in
+          Map.set paths_to_numbers ~key:n ~data:paths_to_n)
+    in
+    Map.find_exn paths_to_numbers final
+  ;;
+end
+
+let%expect_test _ =
+  let input = Part_02.Input.of_string test_case in
+  print_s [%sexp (Part_02.solve input : int)];
+  [%expect {| 8 |}]
+;;
+
+let parts : (module Solution.Part) list = [ (module Part_01); (module Part_02) ]
