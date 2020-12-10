0539e1285dab38a547b76ea24e6e6387f12c5fcd
Levi Roth <levimroth@gmail.com>
2020-12-10 09:11:17 -0500

    Day 10, part 1

---
+++ src/day10.ml
@@ -0,0 +1,44 @@
+open! Core
+open! Import
+
+module Common = struct
+  module Input = Input.Int_list
+  module Output = Int
+end
+
+module Part_01 = struct
+  include Common
+
+  let solve input =
+    let final = Option.value_exn (List.max_elt input ~compare) + 3 in
+    let in_order = List.concat [ [ 0 ]; List.sort input ~compare; [ final ] ] in
+    let pairs, _rest = List.zip_with_remainder (List.drop in_order 1) in_order in
+    let gaps = List.map pairs ~f:(Tuple2.uncurry ( - )) in
+    let counts =
+      List.fold
+        gaps
+        ~init:Int.Map.empty
+        ~f:(Map.update ~f:(Option.value_map ~f:succ ~default:1))
+    in
+    Map.find_exn counts 1 * Map.find_exn counts 3
+  ;;
+end
+
+let%expect_test _ =
+  let test_case = {|16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4|} in
+  let input = Part_01.Input.of_string test_case in
+  print_s [%sexp (Part_01.solve input : int)];
+  [%expect {| 35 |}]
+;;
+
+let parts : (module Solution.Part) list = [ (module Part_01) ]
