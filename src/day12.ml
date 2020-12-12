ebc8c3f7b00c89cf22d3785b089fa0e49254dfa5
Levi Roth <levimroth@gmail.com>
2020-12-12 08:48:27 -0500

    Day 12: deduplicate

--- src/day12.ml
+++ src/day12.ml
@@ -1,6 +1,13 @@
 open! Core
 open! Import
 
+module Change = struct
+  type t =
+    | Move_absolute of Int_pair.t
+    | Move_forward of int
+    | Rotate of (Int_pair.t -> Int_pair.t)
+end
+
 module Instruction = struct
   module Action = struct
     type t =
@@ -37,23 +44,34 @@ module Instruction = struct
     let open Angstrom in
     map2 Action.parser parse_int ~f:(fun action value -> { action; value })
   ;;
+
+  let rotate_right degrees ((x, y) as vector) =
+    match degrees % 360 with
+    | 0 | 360 -> vector
+    | 90 -> y, -x
+    | 180 -> -x, -y
+    | 270 -> -y, x
+    | _ -> raise_s [%message "Degrees is not a multiple of 90" (degrees : int)]
+  ;;
+
+  let rotate_left degrees = rotate_right (360 - degrees)
+
+  let to_change { action; value } : Change.t =
+    match action with
+    | North -> Move_absolute (0, value)
+    | South -> Move_absolute (0, -value)
+    | East -> Move_absolute (value, 0)
+    | West -> Move_absolute (-value, 0)
+    | Forward -> Move_forward value
+    | Left -> Rotate (rotate_left value)
+    | Right -> Rotate (rotate_right value)
+  ;;
 end
 
 module Common = struct
   module Input = Input.Make_parseable_many (Instruction)
   module Output = Int
 end
-
-let rotate_right degrees ((x, y) as vector) =
-  match degrees % 360 with
-  | 0 | 360 -> vector
-  | 90 -> y, -x
-  | 180 -> -x, -y
-  | 270 -> -y, x
-  | _ -> raise_s [%message "Degrees is not a multiple of 90" (degrees : int)]
-;;
-
-let rotate_left degrees = rotate_right (360 - degrees)
 
 module Part_01 = struct
   include Common
@@ -64,13 +82,6 @@ module Part_01 = struct
       ; direction : Int_pair.t
       }
     [@@deriving sexp]
-
-    module Change = struct
-      type t =
-        | Move_absolute of Int_pair.t
-        | Move_forward of int
-        | Rotate of (Int_pair.t -> Int_pair.t)
-    end
 
     let apply t ~(change : Change.t) =
       match change with
@@ -83,24 +94,13 @@ module Part_01 = struct
     ;;
   end
 
-  let to_change ({ action; value } : Instruction.t) : State.Change.t =
-    match action with
-    | North -> Move_absolute (0, value)
-    | South -> Move_absolute (0, -value)
-    | East -> Move_absolute (value, 0)
-    | West -> Move_absolute (-value, 0)
-    | Forward -> Move_forward value
-    | Left -> Rotate (rotate_left value)
-    | Right -> Rotate (rotate_right value)
-  ;;
-
   let solve input =
     let ({ position = x, y; direction = _ } : State.t) =
       List.fold
         input
         ~init:({ position = 0, 0; direction = 1, 0 } : State.t)
         ~f:(fun state instruction ->
-          let change = to_change instruction in
+          let change = Instruction.to_change instruction in
           State.apply state ~change)
     in
     Int.abs x + Int.abs y
@@ -129,13 +129,6 @@ module Part_02 = struct
       }
     [@@deriving sexp]
 
-    module Change = struct
-      type t =
-        | Move_absolute of Int_pair.t
-        | Move_forward of int
-        | Rotate of (Int_pair.t -> Int_pair.t)
-    end
-
     let apply t ~(change : Change.t) =
       match change with
       | Move_absolute vector -> { t with waypoint = Int_pair.add t.waypoint vector }
@@ -147,24 +140,13 @@ module Part_02 = struct
     ;;
   end
 
-  let to_change ({ action; value } : Instruction.t) : State.Change.t =
-    match action with
-    | North -> Move_absolute (0, value)
-    | South -> Move_absolute (0, -value)
-    | East -> Move_absolute (value, 0)
-    | West -> Move_absolute (-value, 0)
-    | Forward -> Move_forward value
-    | Left -> Rotate (rotate_left value)
-    | Right -> Rotate (rotate_right value)
-  ;;
-
   let solve input =
     let ({ position = x, y; waypoint = _ } : State.t) =
       List.fold
         input
         ~init:({ position = 0, 0; waypoint = 10, 1 } : State.t)
         ~f:(fun state instruction ->
-          let change = to_change instruction in
+          let change = Instruction.to_change instruction in
           State.apply state ~change)
     in
     Int.abs x + Int.abs y
