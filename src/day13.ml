c1e9c4ab6ea690e3a949027535c370459d421f16
Levi Roth <levimroth@gmail.com>
2020-12-13 09:11:52 -0500

    Day 13, part 1

---
+++ src/day13.ml
@@ -0,0 +1,52 @@
+open! Core
+open! Import
+
+let test_case = {|939
+        7,13,x,x,59,x,31,19|}
+
+module Notes = struct
+  type t =
+    { earliest_timestamp : int
+    ; buses : int list
+    }
+  [@@deriving sexp]
+
+  let parser =
+    let open Angstrom in
+    let buses =
+      many
+        (parse_int
+        <* take_while (function
+               | ',' | 'x' -> true
+               | _ -> false))
+    in
+    map2
+      (parse_int <* take_while Char.is_whitespace)
+      buses
+      ~f:(fun earliest_timestamp buses -> { earliest_timestamp; buses })
+  ;;
+end
+
+module Part_01 = struct
+  module Input = Input.Make_parseable (Notes)
+  module Output = Int
+
+  let solve ({ earliest_timestamp; buses } : Notes.t) =
+    let time_waited bus = (bus * ((earliest_timestamp / bus) + 1)) - earliest_timestamp in
+    let bus =
+      Option.value_exn
+        (List.min_elt buses ~compare:(Comparable.lift compare ~f:time_waited))
+    in
+    bus * time_waited bus
+  ;;
+
+  let solver = Angstrom.map Input.parser ~f:solve
+end
+
+let%expect_test _ =
+  let input = Part_01.Input.of_string test_case in
+  print_s [%sexp (Part_01.solve input : int)];
+  [%expect {| 295 |}]
+;;
+
+let parts : (module Solution.Part) list = [ (module Part_01) ]
